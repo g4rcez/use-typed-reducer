eec900af6a982cec1eea625f90b7175da86db162
g4rcez <allan.f.garcez@gmail.com>
2020-09-17 19:23:51 -0300

    add middleware to useReducer

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,27 @@ import { Dispatch } from "./typings";
 
 export const useReducer: <State, Reducers extends Dispatch<State, Reducers>>(
     state: State,
-    reducers: Reducers
-) => [State, Dispatch<State, Reducers>] = <State, Reducers>(state: State, reducers: Reducers) => {
+    reducers: Reducers,
+    middlewares: ((state: State) => State)[]
+) => [State, Dispatch<State, Reducers>] = <State, Reducers>(
+    state: State,
+    reducers: Reducers,
+    middlewares: ((state: State) => State)[] = []
+) => {
     const [localState, setLocalState] = useState(state);
     const dispatches = useMemo(() => {
         return Object.entries(reducers).reduce(
             (acc, [name, dispatch]) => ({
                 ...acc,
-                [name]: (...params: unknown[]) => setLocalState((st: State) => dispatch(...params)(st))
+                [name]: (...params: unknown[]) =>
+                    setLocalState((st: State) => {
+                        const newSt = dispatch(...params)(st);
+                        return middlewares.reduce((acc, el) => el(acc), newSt);
+                    })
             }),
             reducers
         );
-    }, [reducers]);
+    }, [reducers, middlewares]);
     return [localState, dispatches] as never;
 };
 
