12f95a0e6746520c0ad099a4ba9de6f22c7c114d
g4rcez <allan.f.garcez@gmail.com>
2024-03-10 17:01:59 -0300

    feat: export plugins

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,9 @@ export type { Debug, Callback, ReducerMiddleware, UseReducer, PromiseBox, MapRed
 export { useReducer, useLegacyReducer, createGlobalReducer } from "./use-typed-reducer";
 export { clone, shallowCompare, isPrimitive, isPromise, dispatchCallback } from "./lib";
 export { usePrevious, useMutable } from "./hooks";
+export {
+    createLocalStoragePlugin,
+    createLoggerPlugin,
+    createSessionStoragePlugin,
+    createStoragePlugin
+} from "./plugins";
